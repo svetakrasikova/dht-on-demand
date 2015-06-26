9048a55f55ea319c1bcbef665255ccc9a25e11c1
Sveta Krasikova <contact@svetakrasikova.eu>
2015-06-26 15:02:23 +0200

    Parametrize #contacts per affinity group

--- tkelips_basic.lua
+++ tkelips_basic.lua
@@ -27,6 +27,13 @@ n = 128
 --number of affinity groups
 k = math.floor(math.sqrt(n))
 
+
+GOSSIP_TIME = 5
+TMAN_RANDOM = 2
+TMAN_AG_SIZE = n/k
+TMAN_MESSAGE_SIZE = 4
+TMAN_CONTACT_SIZE = 2
+
 -------------------------------------------------------------------------------
 -- current node
 -------------------------------------------------------------------------------
@@ -39,23 +46,20 @@ function compute_hash(o)
 end
 
 me.id = compute_hash(table.concat({tostring(job.me.ip),":",tostring(job.me.port)}))
-
 -- affinity group
 ag = me.id%k
 
--------------------------------------------------------------------------------
--- T-MAN
--------------------------------------------------------------------------------
 
-GOSSIP_TIME = 5
-TMAN_RANDOM = 2
-TMAN_AG_SIZE = n/k
-TMAN_MESSAGE_SIZE = 4
+-------------------------------------------------------------------------------
+-- T-KELIPS
+-------------------------------------------------------------------------------
 
 TMAN = {
 	aff_group= {},
 	contacts = {},
+	filetuples = {},
 	t = TMAN_AG_SIZE,
+	c = TMAN_CONTACT_SIZE,
 	r = TMAN_RANDOM,
 	m = TMAN_MESSAGE_SIZE,
 	cycle = 0,
@@ -79,11 +83,30 @@ TMAN = {
 		log:print(out)
 	end,
 	
-	display_contacts = function(c) 
+	--currently only one contact per affinity group
+	display_contacts_old = function(c) 
 		local out = table.concat({"TMAN cycle", " ", c, " ", "APPROX. CONTACTS\t"})
 		for i = 0, k-1 do
 			if TMAN.contacts[i] then
 				out = table.concat({out, i," ",TMAN.contacts[i].id,"(",TMAN.contacts[i].id%k,")\t"})
+			end
+		end
+		log:print(out)
+	end,
+	
+	display_contacts = function(c)
+		local out = table.concat({"TMAN cycle", " ", c, " ", "APPROX. CONTACTS\n"})
+		for i = 0, k-1 do
+			if TMAN.contacts[i] then
+			local str = table.concat({i,": "})
+				for j = 1, TMAN.c do
+					if TMAN.contacts[i][j] then
+						str = table.concat({str," ",TMAN.contacts[i][j].id,"(",TMAN.contacts[i][j].id%k,")"})
+					else
+						str = table.concat({str," -"})
+					end
+				end
+			out = table.concat({out,str,"\n"})
 			end
 		end
 		log:print(out)
@@ -95,7 +118,7 @@ TMAN = {
 	end,
 	
 -------------------------------------------------------------------------------
--- T-MAN utilities
+-- utilities
 -------------------------------------------------------------------------------
 	-- remove duplicates from view
 	remove_dup = function(set)
@@ -125,14 +148,14 @@ TMAN = {
 		end
 	end,
 
-	--ranks the set of nodes from the point of view of the node with the specified id based on their distance on the ring
+	--ranks nodes in set according to the distance on the ring relative to n
 	rank = function(n, set)
 		local distances = {}
 		local ranked = {}
 		for i,v in ipairs(set) do
 			local d = 0
 			local dist_clockwise = math.abs(v.id%k - n.id%k)
-			local dist_counter = TMAN.t - dist_clockwise
+			local dist_counter = k - dist_clockwise
 			if dist_clockwise <= dist_counter then d = dist_clockwise
 			else d = dist_counter end
 			distances[#distances+1] = {distance= d, node=v}
@@ -171,9 +194,7 @@ TMAN = {
 -------------------------------------------------------------------------------
 
 	selectPeer = function()
-		--local sp = misc.time()
 		local partner = TMAN.random_pick(1)[1]
-		--log:print("TMAN.selectPeer", misc.time() - sp, "peer id", partner.id%2^TMAN.t)
 		return partner
 		end,
 
@@ -203,7 +224,14 @@ TMAN = {
 		for i,v in ipairs(received) do
 			local ag = v.id%k
 			if TMAN.contacts[ag] == nil then
-				TMAN.contacts[ag] = v
+				TMAN.contacts[ag] = {}
+			end
+			if #TMAN.contacts[ag] < TMAN.c then
+				local t = {v}
+				local merged = misc.merge(TMAN.contacts[ag],t)
+				TMAN.remove_dup(merged)
+				TMAN.keep_n(merged,TMAN.c)
+				TMAN.contacts[ag] = merged
 			end
 		end
 	end,
