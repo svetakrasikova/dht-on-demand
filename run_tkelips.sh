d34c66bb9fbae1a6ae27493171cf428613042401
Sveta Krasikova <contact@svetakrasikova.eu>
2015-06-26 14:08:15 +0200

    Add bash script to run tkelips locally

---
+++ run_tkelips.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+max=$1
+if [[ $max == "" ]]
+then
+  max=128
+fi
+
+killall lua51
+sleep 1
+
+for (( n=1;n<=$max;n++ ))
+do
+  rm $n.log > /dev/null 2>&1
+lua51 ./tkelips_basic.lua $n $max > $n.log 2>&1 &
+done
