60bd1d2d5db4368ab8343de136ba936d8d4939d6
Sveta Krasikova <contact@svetakrasikova.eu>
2015-07-13 16:16:44 +0200

    Fix processing of view states

--- log_parse/tkelips_convergence.lua
+++ log_parse/tkelips_convergence.lua
@@ -5,38 +5,42 @@ misc = require"splay.misc"
 io.input(arg[1])
 io.output(arg[2])
 
+function timeInSec(h,m,s)
+t = {wday = 1, year = 2003, yday = 124, month = 5, day = 4, isdst = false, hour = tonumber(h),min= tonumber(m),sec = tonumber(s)}
+	return os.time(t)
+end
 
-num_nodes = arg[3]
+num_nodes = tonumber(arg[3])
 map = {}
 complete_states = {}
 
+--parsing the log file line and collecting the number of optional and mandatory entries in the views per second
 for line in io.lines() do
 	--	parse the timestamp and convert it into seconds 
 	h,m,s = string.match(line, "(%d%d):(%d%d):(%d%d)")
 	if sec == nil then
 		first_sec = timeInSec(h,m,s)
 		sec = 0
-	else sec = timeInSec(h,m,s) - first_sec end
+	else
+		next_sec = timeInSec(h,m,s) - first_sec
+		if next_sec < 0 then sec = 0 else sec = next_sec end
+	end
+	
 	if not map[sec] then map[sec] = {} end
 	
 	--collect complete view states
 	if string.match(line, "COMPLETE VIEW STATE") then
 		local id, mandatory, optional = string.match(line, "STATE.-(%d+).+mandatory_entries:(%d+).+optional_entries:(%d+)")
-		if not complete_states[id] then complete_states[id] = {m = tonumber(mandatory), o = tonumber(optional)}
+		if not complete_states[id] then complete_states[id] = {m = tonumber(mandatory), o = tonumber(optional)} end
 	end
 	
 	--collect view states per second
 	if string.match(line, "CURRENT VIEW STATE") then
 		local id, mandatory, optional = string.match(line, "STATE.-(%d+).+mandatory_entries:(%d+).+optional_entries:(%d+)")
-		map[sec].id = {m = mandatory, o = optional}
-	end
-	
+		map[sec][id] = {m = mandatory, o = optional}
+	end	
 end
 
-function timeInSec(h,m,s)
-t = {wday = 1, year = 2003, yday = 124, month = 5, day = 4, isdst = false, hour = tonumber(h),min= tonumber(m),sec = tonumber(s)}
-	return os.time(t)
-end
 
 function process_complete_states()
 	n = 0
@@ -54,7 +58,7 @@ end
 function process_view_states(map, mand, opt)
 	stats = {}
 	for i,v in pairs(map) do
-		stats[i] = {m = 0, o = 0}
+		if not stats[i] then stats[i] = {m = 0, o = 0}; end
 		if previous then
 			v = misc.merge(v,previous)
 		end
@@ -69,9 +73,10 @@ function process_view_states(map, mand, opt)
 	return stats
 end
 
-complete = process_complete_states()
-stats = process_complete_states(map, complete[1], complete[2])
-io.write("SECOND\tMANDATORY\tOPTIONAL\tMANDATORY PERCENTAGE\tOPTIONAL PERCENTAGE")
+complete = {process_complete_states()}
+stats = process_view_states(map, complete[1], complete[2])
+
+io.write("SECOND\tMANDATORY\tOPTIONAL\tMANDATORY PERCENTAGE\tOPTIONAL PERCENTAGE\n")
 for i,v in pairs(stats) do
 	io.write(i.."\t"..v.m.."\t"..v.o.."\t"..v.m_percent.."\t"..v.o_percent.."\n")
 end
